fe7776a3a70b949a9edd44cd0c4e9389188d87aa
Phùng Văn Duy <phungvanduy23052005@gmail.com>
2026-07-17 21:43:30 +0700

    feat: add card for  body and style color text

--- flutter-lab-1/lib/main.dart
+++ flutter-lab-1/lib/main.dart
@@ -37,77 +37,121 @@ class WebcomeApp extends StatelessWidget {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.teal.shade50,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.teal, width: 3),
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  color: Colors.teal.shade50,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.teal, width: 3),
                 ),
-                const SizedBox(height: 20),
-                const Text(
-                  'PHÙNG VĂN DUY - MSV: 23520275',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'PHÙNG VĂN DUY - MSV: 2351170589',
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  textBaseline: TextBaseline.alphabetic,
                 ),
-                const SizedBox(height: 8),
-                const Text(
-                  'Khoa Công nghệ Thông tin - Đại học Thủy Lợi Khóa học Phát triển ứng dụng Mobile (CSE441)',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20, color: Colors.blue),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Khoa Công nghệ Thông tin - Đại học Thủy Lợi Khóa học Phát triển ứng dụng Mobile (CSE441)',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black45,
+                  textBaseline: TextBaseline.alphabetic,
                 ),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.rocket_launch_rounded,
+              ),
+              const SizedBox(height: 20),
+              Card(
+                elevation: 9,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
                           color: Colors.orange,
+                          shape: BoxShape.circle,
                         ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Môi trường Flutter 3.19+",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              const SizedBox(width: 4),
-                              Text(
-                                "'Đã sẵn sàng chinh phục 15 bài thực hành!'",
-                              ),
-                            ],
-                          ),
+                        child: const Icon(
+                          Icons.rocket_launch_rounded,
+                          color: Colors.white,
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Môi trường Flutter 3.19+",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text("Đã sẵn sàng chinh phục 15 bài thực hành!"),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              Card(
+                color: Colors.blue[50],
+                elevation: 9,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+
+                  side: BorderSide(color: Colors.blue, width: 1),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.h_mobiledata, color: Colors.black),
+                          const SizedBox(width: 10),
+                          Text(
+                            "Thông tin cá nhân",
+
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text("Tiêu chuẩn đầu ra ABED - khoa CNTT - TLU"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
       ),
     );
   }
